84513d2ce944b7a445949304a3c2093b2a27aae3
trimute2 <ben.j.stern@gmail.com>
2019-03-03 22:30:34 -0500

    created project, player can fire at an object and it will disapear

---
+++ Assets/Scripts/CanShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanShoot : MonoBehaviour {
+	public float range;
+	Camera fpsCamera;
+	// Use this for initialization
+	void Start () {
+		fpsCamera = GetComponentInChildren<Camera>();
+	}
+
+	public void Fire()
+	{
+		Vector3 rayOrigin = fpsCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+		RaycastHit raycast;
+		if(Physics.Raycast(rayOrigin, fpsCamera.transform.forward, out raycast, range))
+		{
+			if (raycast.collider.CompareTag("Target")){
+				Destroy(raycast.collider.gameObject);
+			}
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Fire();
+		}
+	}
+}
